c37b69107633bb0d7cbe0c84f35e180ce819a2ef
Facundo Gerry <facuto365pro@gmail.com>
2026-04-19 19:17:28 -0300

    Add Heal method to HealthSystem

--- Assets/Scripts/HealthSystem.cs
+++ Assets/Scripts/HealthSystem.cs
@@ -28,4 +28,12 @@ public class HealthSystem : MonoBehaviour
             _durability = 0;
         Debug.Log("car has " + _durability + " left");
     }
+
+    private void Heal(int heal)
+    {
+        _durability += heal;
+        if (_durability > _data.maxDurability)
+            _durability = _data.maxDurability;
+        Debug.Log("car healed and now has " + _durability + " left");
+    }
 }
