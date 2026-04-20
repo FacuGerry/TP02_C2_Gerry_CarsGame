d7e46cbccec0e328d27008ec7af09b88e450386f
Facundo Gerry <facuto365pro@gmail.com>
2026-04-19 22:27:21 -0300

    Add UiSpeed, Add UiGas

---
+++ Assets/Scripts/UI/UiGasUsage.cs
@@ -0,0 +1,22 @@
+using TMPro;
+using UnityEngine;
+
+public class UiGasUsage : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _text;
+
+    private void OnEnable()
+    {
+        GasSystem.OnGasChange += ChangeGas;
+    }
+
+    private void OnDisable()
+    {
+        GasSystem.OnGasChange -= ChangeGas;
+    }
+
+    private void ChangeGas(float gas)
+    {
+        _text.text = "GAS: " + gas.ToString();
+    }
+}
---
+++ Assets/Scripts/UI/UiSpeedometer.cs
@@ -0,0 +1,22 @@
+using TMPro;
+using UnityEngine;
+
+public class UiSpeedometer : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _text;
+
+    private void OnEnable()
+    {
+        CarMovement.OnSpeedChange += ChangeSpeed;
+    }
+
+    private void OnDisable()
+    {
+        CarMovement.OnSpeedChange -= ChangeSpeed;
+    }
+
+    private void ChangeSpeed(int speed)
+    {
+        _text.text = "SPEED: " + speed.ToString();
+    }
+}
