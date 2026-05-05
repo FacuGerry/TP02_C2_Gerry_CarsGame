b22366786e983e915c93ef4d3659053b4f73df42
Facundo Gerry <facuto365pro@gmail.com>
2026-05-05 00:15:23 -0300

    Remove extra scripts from imports

--- Assets/ScriptsNew/Data/GameDataSO.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "GameData", menuName = "GameSettings/GameData")]
-
-public class GameDataSO : ScriptableObject
-{
-    public int minEnemies;
-    public int maxEnemies;
-}
