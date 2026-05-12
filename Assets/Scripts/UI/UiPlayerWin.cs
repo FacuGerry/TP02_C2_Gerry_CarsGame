4d26ae0473ba94152e8f7dff9545fdd7423f995f
Facundo Gerry <facuto365pro@gmail.com>
2026-05-12 17:02:12 -0300

    Add Button to go to MainMenu from winScreen

--- Assets/Scripts/UI/UiPlayerWin.cs
+++ Assets/Scripts/UI/UiPlayerWin.cs
@@ -1,12 +1,28 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UiPlayerWin : MonoBehaviour
 {
+    [SerializeField] private Button _btn;
+    [SerializeField] private string _sceneToLoad = "MainMenu";
     private CanvasGroup _canvas;
 
     private void Start()
     {
         ChangeCanvas(false);
+
+        _btn.onClick.AddListener(GoToMainMenu);
+    }
+
+    private void OnDestroy()
+    {
+        _btn.onClick.RemoveAllListeners();
+    }
+
+    private void GoToMainMenu()
+    {
+        SceneManager.LoadScene(_sceneToLoad);
     }
 
     public void OnWin_SetCanvas()
